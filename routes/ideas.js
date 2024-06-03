2457ede972dc87623597abd69242f067dac2e464
TonyAppiah <appiah.tony@gmail.com>
2024-06-03 11:23:14 +0300

    Added the Body Parser middleware to server.js in order to create POST, PUT and DELETE request functionality in ideas.js

--- routes/ideas.js
+++ routes/ideas.js
@@ -51,4 +51,50 @@ router.get("/:id", (request, response) => {
   response.send({ success: true, data: idea });
 });
 
+//Add an idea
+router.post("/", (request, response) => {
+  const idea = {
+    id: ideas.length + 1,
+    text: request.body.text,
+    tag: request.body.tag,
+    username: request.body.username,
+    date: new Date().toISOString().slice(0, 10),
+  };
+
+  ideas.push(idea);
+
+  response.send({ success: true, data: idea });
+});
+
+//update an idea
+router.put("/:id", (request, response) => {
+  const idea = ideas.find((idea) => idea.id === +request.params.id);
+
+  if (!idea) {
+    return response
+      .status(404)
+      .json({ success: false, error: "Idea not found." });
+  }
+
+  idea.text = request.body.text || idea.text;
+  idea.tag = request.body.tag || idea.tag;
+
+  response.send({ success: true, data: idea });
+});
+
+//Delete an idea
+router.delete("/:id", (request, response) => {
+  const idea = ideas.find((idea) => idea.id === +request.params.id);
+
+  if (!idea) {
+    return response
+      .status(404)
+      .json({ success: false, error: "Idea not found." });
+  }
+
+  ideas.splice(idea, 1);
+
+  response.send({ success: true, data: {} });
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+//Body parser middleware :this is so data can be sent when a post request is made
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (request, response) => {
   response.send("Hello world");
 });
